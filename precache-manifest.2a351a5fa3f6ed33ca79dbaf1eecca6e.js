60dc1a3a04f1edbc94d0cd5025b7d0f548903104
Nelson Lim <30296067+nelsonlim96@users.noreply.github.com>
2020-02-23 22:14:24 +0800

    Updates

---
+++ precache-manifest.2a351a5fa3f6ed33ca79dbaf1eecca6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77d400dbadc7e8eb9e7fa629d8a966b3",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "5a9aebebe14de7e686c0",
+    "url": "/hangman/static/css/main.6cfca161.chunk.css"
+  },
+  {
+    "revision": "d967a99b043064617b09",
+    "url": "/hangman/static/js/2.9e169642.chunk.js"
+  },
+  {
+    "revision": "5a9aebebe14de7e686c0",
+    "url": "/hangman/static/js/main.e8828189.chunk.js"
+  },
+  {
+    "revision": "6832b9ee94d73d9c9b04",
+    "url": "/hangman/static/js/runtime~main.fbdbb0c9.js"
+  }
+]);
